75cb21c38a74ac6ccde28527ff1950c08351a032
mmirtir <qqmen017@gmail.com>
2025-04-22 17:47:42 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi! I am Ksusha from Wawa 🐍");
+        System.out.println("This is my snake:");
+        System.out.println("~@~@~@~@~@~@~@~@~@~");
+    }
+}
